bd999e7f78b12d0956d511c1fa36f0f38a124763
sa5857 <sa5857@gmail.com>
2026-07-08 00:33:24 -0300

    Primeiro commit - Automação de cadastro de produtos com PyAutoGUI

---
+++ codigo.py
@@ -0,0 +1,5 @@
+# Passo 1 - entrar no sistema da empresa
+# Passo 2 - fazer login
+# Passo 3 - abrir a base de dados
+# Passo 4 - cadastrar um produto
+# Passo 5 - repetir o passo quatro ate acabar a lista de produtos
