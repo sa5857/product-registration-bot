1c05799e58b3793ec599861b11afb3a181ed9771
sa5857 <sa5857@gmail.com>
2026-08-26 21:48:56 -0300

    Colocado instrução para que o pyautogui escreva o link da página que se deseja abrir

--- codigo.py
+++ codigo.py
@@ -8,10 +8,12 @@ import pyautogui
 #inteligência do algoritmo
 # Passo 1 - entrar no sistema da empresa (abrir o navegador)
 pyautogui.PAUSE = 2
+link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
 # Passo 1 - entrar no sistema da empresa
 pyautogui.press("win")
 pyautogui.write("chrome")
 pyautogui.press("enter")
+pyautogui.write(link)
 
 # Passo 2 - fazer login
 # Passo 3 - abrir a base de dados
