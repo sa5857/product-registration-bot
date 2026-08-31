92c448f84f4deddb4547df047372a0493a753a89
sa5857 <sa5857@gmail.com>
2026-08-30 21:35:34 -0300

    Acrescentado instrução pyautogui.click para o mause clicar no campo de email da página do sistema

--- codigo.py
+++ codigo.py
@@ -8,7 +8,7 @@ import time
 
 #inteligência do algoritmo
 # Passo 1 - entrar no sistema da empresa (abrir o navegador)
-pyautogui.PAUSE = 2
+pyautogui.PAUSE = 0.5
 link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
 # Passo 1 - entrar no sistema da empresa
 pyautogui.press("win")
@@ -16,12 +16,14 @@ pyautogui.write("chrome")
 pyautogui.press("enter")
 pyautogui.write(link)
 pyautogui.press("enter")
-time.sleep(3)
-
 
 #fazer uma pausa maior para o site carregar
+time.sleep(3)
 
 # Passo 2 - fazer login
+#clicar no campo de email
+pyautogui.click(x=597, y=508)
+
 # Passo 3 - abrir a base de dados
 # Passo 4 - cadastrar um produto
 # Passo 5 - repetir o passo quatro ate acabar a lista de produtos
