ebba37955878ea403c71e99349e47e5d80c392f5
sa5857 <sa5857@gmail.com>
2026-09-01 19:58:06 -0300

    Inserido instrução para o sistema prescionar tab e mudar para o campo de senha. Tb foi inserido o pyautogui.hotkey para forçar a seleção da barra de endereços do chrome

--- codigo.py
+++ codigo.py
@@ -8,22 +8,31 @@ import time
 
 #inteligência do algoritmo
 # Passo 1 - entrar no sistema da empresa (abrir o navegador)
-pyautogui.PAUSE = 0.5
+pyautogui.PAUSE = 3
 link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
 # Passo 1 - entrar no sistema da empresa
 pyautogui.press("win")
 pyautogui.write("chrome")
-pyautogui.press("enter")
+pyautogui.press("enter") 
+
+# FORÇA O FOCO NA BARRA DE ENDEREÇO
+# Método 1: Usar atalho do Chrome (Ctrl+L ou F6)
+pyautogui.hotkey("ctrl", "l")  # Seleciona a barra de endereço
+# Ou use: pyautogui.press("f6")
+
+
 pyautogui.write(link)
 pyautogui.press("enter")
 
-#fazer uma pausa maior para o site carregar
+#fazer uma pausa maior para o site carregar 
 time.sleep(3)
 
 # Passo 2 - fazer login
 #clicar no campo de email e digitar o email
 pyautogui.click(x=597, y=508)
 pyautogui.write("pythonimpressionador@gmail.com")
+#passar para o próximo campo
+pyautogui.press("tab")
 
 # Passo 3 - abrir a base de dados
 # Passo 4 - cadastrar um produto
