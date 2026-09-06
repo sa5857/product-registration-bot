5d144675f20423863dd055437b097a10448fbe66
sa5857 <sa5857@gmail.com>
2026-09-05 23:28:42 -0300

    comando print para mostrar o conteúdo da tabela

--- codigo.py
+++ codigo.py
@@ -9,14 +9,14 @@ import pandas
 
 #inteligência do algoritmo
 # Passo 1 - entrar no sistema da empresa (abrir o navegador)
-pyautogui.PAUSE = 3
+pyautogui.PAUSE = 2
 link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
 # Passo 1 - entrar no sistema da empresa
 pyautogui.press("win")
 pyautogui.write("chrome")
 pyautogui.press("enter")
 
-# FORÇA O FOCO NA BARRA DE ENDEREÇO
+# Força o foco na barra de endereço
 # Método 1: Usar atalho do Chrome (Ctrl+L ou F6)
 pyautogui.hotkey("ctrl", "l")  # Seleciona a barra de endereço
 # Ou use: pyautogui.press("f6")
@@ -25,7 +25,7 @@ pyautogui.hotkey("ctrl", "l")  # Seleciona a barra de endereço
 pyautogui.write(link)
 pyautogui.press("enter")
  
-time.sleep(3)  #fazer uma pausa maior para o site carregar
+time.sleep(1)  #fazer uma pausa maior para o site carregar
 
 # Passo 2 - fazer login
 pyautogui.click(x=597, y=508) #clicar no campo de email e digitar o email
@@ -34,8 +34,14 @@ pyautogui.press("tab") #passar para o próximo campo
 pyautogui.write("123456") #digitar a senha
 pyautogui.press("tab") #pular para o botão
 pyautogui.press("enter") #apertar o botão
-pyautogui.sleep(3) #uma pausa maior para carregar a página
+pyautogui.sleep(1) #uma pausa maior para carregar a página
+
 
 # Passo 3 - abrir a base de dados (importar o arquivo)
+# pipi install pandas openpyxl
+
+
+tabela = pandas.read_csv("tabela.csv")
+print(tabela)
 # Passo 4 - cadastrar um produto
-# Passo 5 - repetir o passo quatro ate acabar a lista de produtos
+# Passo 5 - repetir o passo quatro ate acabar a lista de produtospythonimpressionador@gmail.com 123456  
